3c27580b89b4061a2ee08cf6de52028108c6cfd0
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 17:18:39 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
                 :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attributes)
-    attributes.each {
+    attributes.each {|key, value| 
   end 
   
 end
